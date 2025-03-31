51db8d9ad6370dde630ace2d4aeb191922a15a54
micha <michael.andrew.glass@gmail.com>
2025-03-31 23:10:51 +0100

    added page titles

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  if (req.path === '/style.css') {
+    res.setHeader('Content-Type', 'text/css');
+  }
+  next();
+});
 
-// Import routes
-const indexRoutes = require('./routes/index');
-app.use('/', indexRoutes);
+app.get('/', (req, res) => {
+    res.render('index'); 
+  });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`BNN app running on port ${PORT}`));
